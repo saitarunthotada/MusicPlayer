61143c1571d394f37f278205e759b0d92dc65ffc
saitarun thotada <thotadasaitarun1@gmail.com>
2024-09-03 20:46:53 +0530

    Initial commit

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,7 +3,6 @@
 /** Dashboard Component
   * This component serves as the main interface for the music player app.
   * It is styled with a dark theme using Material-UI's theming capabilities**/
-
 import React, { useState } from 'react';
 import { Container, Paper, Typography, Box, CssBaseline, Grid } from '@mui/material';
 import SongList from '../components/Songs/SongList';
@@ -40,7 +39,7 @@ function Dashboard() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Container>
+      <Container maxWidth="lg">
         <Box my={4}>
           <Paper elevation={3} sx={{ padding: 2, borderRadius: '16px', display: 'flex', flexDirection: 'column', height: '100vh' }}>
             <Typography variant="h4" gutterBottom>
@@ -48,14 +47,14 @@ function Dashboard() {
             </Typography>
             <Grid container spacing={2}>
               <Grid item xs={12} md={8}>
-                <Paper elevation={0} sx={{ borderRadius: '16px', height: 'calc(100% - 150px)', overflow: 'hidden' }}>
+                <Paper elevation={0} sx={{ borderRadius: '16px', height: { xs: 'auto', md: 'calc(100% - 150px)' }, overflow: 'auto' }}>
                   <SongList onPlay={playSong} />
                 </Paper>
               </Grid>
               <Grid item xs={12} md={4}>
                 <Box p={2} bgcolor="background.paper" borderRadius="16px" height="100%" display="flex" flexDirection="column">
                   {currentSong && (
-                    <Box mb={2} p={2} bgcolor="background.paper" borderRadius="16px" flex="1">
+                    <Box mb={2} p={2} bgcolor="background.paper" borderRadius="16px" flex="1" overflow="auto">
                       <Typography variant="h5" gutterBottom>Now Playing</Typography>
                       <Typography variant="h6">{currentSong.title} - {currentSong.artist}</Typography>
                       <audio controls style={{ width: '100%' }}>
@@ -64,7 +63,7 @@ function Dashboard() {
                       </audio>
                     </Box>
                   )}
-                  <Box>
+                  <Box display="flex" flexDirection="column" gap={2}>
                     <AddSong />
                     <PlaylistList />
                     <CreatePlaylist />
